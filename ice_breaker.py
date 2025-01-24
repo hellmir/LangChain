c9ca5cca58023f53c850c234731dd6069d992bf2
hellmir <hellmir610@gmail.com>
2025-01-24 17:56:18 +0900

    [LCLN-014] feat(llama): Add llama3.2 service to project (#4)
    - ice_breaker.py
    - Pipfile
    - Pipfile.lock

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,8 +1,22 @@
-import os
+from langchain_core.prompts import PromptTemplate
+from langchain_ollama import ChatOllama
 
-from dotenv import load_dotenv
+information = "토마토, 밀가루"
 
 if __name__ == "__main__":
-    load_dotenv()
     print("Hello LangChain!")
-print(os.environ["COOL_API_KEY"])
+
+summary_template = """
+    Given the information {information} about ingredients from I want you to create:
+    1. Several recommendations of recipe from the ingredients
+    2. Answer in KOREAN
+    """
+
+summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+llm = ChatOllama(model="llama3.2")
+
+chain = summary_prompt_template | llm
+
+res = chain.invoke(input={"information": information})
+
+print(res)
